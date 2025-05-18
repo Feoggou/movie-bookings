af84630b911d51ccbf41f0e09ae4b571200774f2
Samuel Ghinet <samuel.ghinet@gmail.com>
2025-05-18 16:01:35 +0300

    Rename MovieBooking -> src

---
+++ MovieBooking/src/main.cpp
@@ -0,0 +1,12 @@
+﻿// MovieBooking.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
