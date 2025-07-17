55a251fb4c29a93bfb96185c72dc73eec4d11f3f
Samuel Ghinet <samuel.ghinet@gmail.com>
2025-07-17 16:56:15 +0300

    Remove zeromq_async from lib -- I moved it to main app

--- MovieBooking/lib/src/zeromq_async.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <functional>
-#include <string>
-
-void zeromq_async_main(std::function<void(std::string_view, std::string_view)> process_request);
-void zeromq_async_reply(std::string_view client_id, std::string_view reply_msg);
